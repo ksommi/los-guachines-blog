ab94f953e1086d203c3f0f7ca3814f768c8098dc
ksommi <39311989+ksommi@users.noreply.github.com>
2024-10-28 00:26:51 -0300

    update og-image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,8 +12,6 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   /* { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' }, */
 ]
 
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
-
 const Header = ({ titlePre = '' }) => {
   const { pathname } = useRouter()
 
@@ -26,10 +24,10 @@ const Header = ({ titlePre = '' }) => {
           content="An example Next.js site using Notion for the blog"
         />
         <meta name="og:title" content="Los Guachines" />
-        <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:image" content="/og-image.png" />
         <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="twitter:image" content="/og-image.png" />
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
